36bfb5c8c8ee233b1520bce4af78ee7d3136a903
Victor Landim <victorlandim5@gmail.com>
2019-05-18 12:41:46 -0300

    Updates

---
+++ precache-manifest.8422d56891bf468b0501236ce7e05795.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "284218202b60202003eb745992aca224",
+    "url": "/unform-challenge/index.html"
+  },
+  {
+    "revision": "0aad3c5689ab338bca3a",
+    "url": "/unform-challenge/static/css/main.a3fb8aa5.chunk.css"
+  },
+  {
+    "revision": "a80805c06799396b7b12",
+    "url": "/unform-challenge/static/js/2.757d2875.chunk.js"
+  },
+  {
+    "revision": "0aad3c5689ab338bca3a",
+    "url": "/unform-challenge/static/js/main.ee57419a.chunk.js"
+  },
+  {
+    "revision": "ea669cca0f8f471a949f",
+    "url": "/unform-challenge/static/js/runtime~main.d47dbee1.js"
+  }
+]);
